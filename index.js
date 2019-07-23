f97e9f68ac5cdd814984e754a009344db4840948
nabilFurmoli <nabilfurmoli@gmail.com>
2019-07-23 12:16:48 -0700

    working on passport phase1

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 
-const express = require('express');
 
+let appClientId = '934781814221-89r6hem5lj3pf2hv4vdf9k6q854kshuj.apps.googleusercontent.com';
+let appClientSecret = 'SlzBuqhyUpQkTBp5MDS9gBam';
+
+const express = require('express');
+const keys = require('./config/secrets');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+
+passport.use(new GoogleStrategy(
+    {
+       clientID = keys.appGoogleClientID,
+       clientSecret = keys.appGoogleClientSecret,
+       callbackURL = '/auth/google/callback' 
+    }, (accessToken) => {
+        console.log(accessToken);
+    }
+));
 
 app.get('/', (req, res) => {
     res.send({age: 22});
