9a325212b9fe635f517c3435fdd5382785d07b54
nabilFurmoli <nabilfurmoli@gmail.com>
2019-07-24 23:31:23 -0700

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -6,9 +6,3 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
-
-
-// GoogleClientID: '194332329892-jtfslvibrfive5arkfqbgni147n5jcg6.apps.googleusercontent.com',
-//     GoogleClientSecret: 'mss3RhQwG1gGsD5CGH-GKPrp',
-//     mongoURI: 'mongodb+srv://nabil_furmoli_prod:DLboy7EOk9wx0h0N@surveyfeed-prod-56kv4.mongodb.net/test?retryWrites=true&w=majority',
-//     cookieKey: 'nabiljanqandshirinnabatklola-poduction'
