9466abaedcb2ec7432e09a00293ea419592778cb
nabilFurmoli <nabilfurmoli@gmail.com>
2019-08-18 15:33:50 -0700

    added designed changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import Header from "./Header";
-import Dashboard from "./Dashboard";
+import Dashboard from "./pages/Dashboard";
 import IntroPage from "./reusable/IntroPage";
-import CreateSurvey from "./CreateSurvey";
+import CreateSurvey from "./pages/surveys/CreateSurvey";
 
 class App extends Component {
   constructor(props) {
