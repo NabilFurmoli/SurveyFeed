941eafcff3f94ffc3765379488e7f9f58d0205bc
nabilFurmoli <nabilfurmoli@gmail.com>
2019-08-20 21:01:05 -0700

    Update Mailer.js

--- services/Mailer.js
+++ services/Mailer.js
@@ -60,36 +60,3 @@ class Mailer extends helper.Mail {
 module.exports = Mailer;
 
 
-
-// const sgMail = require('@sendgrid/mail'); // separate Node package
-// const helpers = require('@sendgrid/helpers'); // separate Node package
-// const keys = require('../config/secrets'); // some place where you store your API keys
-
-// class Mailer extends helpers.classes.Mail {
-//     // Through the use of Static methods from the Mail helper Class, you create a sendgrid compliant instance that can be send easily
-//     constructor({ subject, recipients }, content) {
-//         super();
-//         this.setFrom('no-reply@emaily.com'); // uses the EmailAddress.create method
-//         this.setSubject(subject);
-//         this.addHtmlContent(content); // same as addContent, but more specific for HTML
-
-//         this.recipients = recipients.map(({ email }) =>
-//             helpers.classes.EmailAddress.create(email)
-//         );
-
-//         this.setTrackingSettings({
-//             clickTracking: { enable: true, enableText: true },
-//         });
-
-//         this.addTo(this.recipients); // This uses the personalization method in the background
-//     }
-
-//     // To separate our data from what we send out, we create another function
-//     async send() {
-//         sgMail.setApiKey(keys.sendGridKey);
-
-//         return await sgMail.send(this); // attach the current instance to be send out with SendGrid
-//     }
-// }
-
-// module.exports = Mailer;
